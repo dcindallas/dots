1945b4e2173b7741859a071a420257045fb6904f
dcindallas <duanecummins@gmail.com>
2022-09-22 13:10:40 -0500

    Updates for the week

--- polybar/scripts/scroll_spotify_status.sh
+++ polybar/scripts/scroll_spotify_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # see man zscroll for documentation of the following parameters
-zscroll -l 30 \
+zscroll -l 27 \
         --delay 0.1 \
         --scroll-padding " " \
         --match-command "`dirname $0`/get_spotify_status.sh --status" \
