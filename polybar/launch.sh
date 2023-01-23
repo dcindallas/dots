2d9e6eafa40fec1aef47739982d113d8518cdd0b
DC <67523002+dcindallas@users.noreply.github.com>
2023-01-23 04:01:44 -0600

    update for new theme

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,12 +11,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-
-polybar -q main -c "$DIR"/config.ini &
-# polybar -q music -c "$DIR"/config.ini &
-
-#polybar -q center -c "$DIR"/config.ini &
-#polybar -q left -c "$DIR"/bar-2/config-2.ini &
-#polybar -q right -c "$DIR"/config.ini &
-#polybar -q music -c "$DIR"/config.ini &
-#polybar -q stats -c "$DIR"/config.ini &
+polybar -q center -c "$DIR"/config.ini &
+polybar -q left -c "$DIR"/config.ini &
+polybar -q right -c "$DIR"/config.ini &
